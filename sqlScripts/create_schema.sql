8e1739066e47e5ae60632f53330206ef90a3a593
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-02-08 13:28:46 -0500

    Update create_schema.sql

--- sqlScripts/create_schema.sql
+++ sqlScripts/create_schema.sql
@@ -1,1 +1,7 @@
 create database hibernate_assignment;
+select * from users u
+left join user_account ua on ua.user_id = u.user_id
+left join accounts a on a.account_id = ua.account_id;
+
+select * from address;
+
