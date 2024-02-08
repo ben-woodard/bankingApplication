544f3cf81def3c65322a28252bdb418b39a95a1a
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-02-08 13:05:21 -0500

    Update AccountService.java

--- src/main/java/com/coderscampus/assignment13/service/AccountService.java
+++ src/main/java/com/coderscampus/assignment13/service/AccountService.java
@@ -6,8 +6,6 @@ import com.coderscampus.assignment13.repository.AccountRepository;
 import com.coderscampus.assignment13.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.Optional;
 
 @Service
